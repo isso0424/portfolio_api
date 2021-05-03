633f932ecbbbdb0c02fb36f590acbfa4c2022f26
isso0424 <isso04240129@gmail.com>
2021-05-03 14:23:47 +0900

    fix: product repository fix to return pointer and error

--- mem_db/product.go
+++ mem_db/product.go
@@ -1,12 +1,14 @@
 package mem_db
 
-import "github.com/isso0424/portfolio_api/domain"
+import (
+	"github.com/isso0424/portfolio_api/domain"
+)
 
 type ProductDB struct {
 	data []domain.Product
 }
 
-func (db *ProductDB) Add(title, description string, tags []string) domain.Product {
+func (db *ProductDB) Add(title, description string, tags []string) (*domain.Product, error) {
 	newProduct := domain.Product{
 		Title:       title,
 		Description: description,
@@ -19,37 +21,37 @@ func (db *ProductDB) Add(title, description string, tags []string) domain.Produc
 		db.data = append(db.data, newProduct)
 	}
 
-	return newProduct
+	return &newProduct, nil
 }
 
-func (db *ProductDB) Delete(title string) domain.Product {
+func (db *ProductDB) Delete(title string) (*domain.Product, error) {
 	if exist, index, product := db.searchByTitle(title); exist {
 		db.data = append(db.data[:index], db.data[index+1:]...)
 
-		return product
+		return product, nil
 	}
 
-	return domain.Product{}
+	return nil, nil
 }
 
-func (db *ProductDB) GetAll() []domain.Product {
-	return db.data
+func (db *ProductDB) GetAll() ([]domain.Product, error) {
+	return db.data, nil
 }
 
-func (db *ProductDB) GetByTitle(title string) domain.Product {
+func (db *ProductDB) GetByTitle(title string) (*domain.Product, error) {
 	if exist, _, product := db.searchByTitle(title); exist {
-		return product
+		return product, nil
 	}
 
-	return domain.Product{}
+	return nil, nil
 }
 
-func (db *ProductDB) searchByTitle(title string) (bool, int, domain.Product) {
+func (db *ProductDB) searchByTitle(title string) (bool, int, *domain.Product) {
 	for index, product := range db.data {
 		if product.Title == title {
-			return true, index, product
+			return true, index, &product
 		}
 	}
 
-	return false, -1, domain.Product{}
+	return false, -1, nil
 }
--- repository/product.go
+++ repository/product.go
@@ -3,8 +3,8 @@ package repository
 import "github.com/isso0424/portfolio_api/domain"
 
 type ProductRepository interface {
-	Add(title, description string, tags []string) domain.Product
-	Delete(title string) domain.Product
-	GetAll() []domain.Product
-	GetByTitle(title string) domain.Product
+	Add(title, description string, tags []string) (*domain.Product, error)
+	Delete(title string) (*domain.Product, error)
+	GetAll() ([]domain.Product, error)
+	GetByTitle(title string) (*domain.Product, error)
 }
