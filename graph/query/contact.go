b14dd0df5fca6acc576ab2c779fd7dc58208181e
isso0424 <isso04240129@gmail.com>
2021-05-11 09:00:59 +0900

    fix: update contact fetcher

--- graph/query/contact.go
+++ graph/query/contact.go
@@ -1,6 +1,9 @@
 package query
 
 import (
+	"errors"
+	"log"
+
 	"github.com/graphql-go/graphql"
 	"github.com/isso0424/portfolio_api/graph/variables"
 	"github.com/isso0424/portfolio_api/types"
@@ -9,6 +12,12 @@ import (
 var FetchContact = &graphql.Field{
 	Type: graphql.NewList(types.Contact),
 	Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-		return variables.ContactDB.GetAll()
+		contacts, err := variables.ContactDB.GetAll()
+		if err != nil {
+			log.Println("error: ", err)
+			return nil, errors.New("internal server error")
+		}
+
+		return contacts, nil
 	},
 }
